2074d3f0001424cac8f259c226855af85fb0f07c
sanil <sanil.pande@gmail.com>
2020-04-18 06:01:58 -0400

    init sub & pub in main

--- src/obstacle_2d/src/obstacle_2d.cpp
+++ src/obstacle_2d/src/obstacle_2d.cpp
@@ -5,21 +5,23 @@
 
 class Chassis{
     public:
-
-        ros::NodeHandle n;
-        ros::Subscriber lidar_sub = n.subscribe("scan", 1000, &Chassis::obstacle_detection_callback, this);
-        ros::Publisher obstacle_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+        ros::Subscriber lidar_sub;
+        ros::Publisher obstacle_pub;
 
         Chassis(){
             // calculate maximum distance to detect obstacles
             double buffer_time = sense_buffer + comm_buffer + safe_buffer;
             double stopping_distance = (vel * vel * 0.5 / acc) + (buffer_time * vel);
+
             total_distance = stopping_distance + lidar_distance + safe_distance;
-            std::cout << total_distance << std::endl;
+            std::cout << "Calculated distance: " << total_distance << std::endl;
+
             double angle = atan2(width, 2 * total_distance);
-            std::cout << angle * 180 / 3.14 << std::endl;
+            std::cout << "Calculated angle: " << angle * 180 / 3.14 << std::endl;
+
             double range = 2.3561899662;
             double start = (range - angle) / 0.0065540750511;
+
             int s = int(start);
             for (int i = s; i < 720-s; i++){
                 range_array[i] = total_distance;
@@ -31,14 +33,14 @@ class Chassis{
             // iterating over all values since ideally all element in range should be non zero
             for (int i = 0; i < 720; i++){
                 if (range_array[i] > scan.ranges[i]){
-                    std::cout << scan.ranges[i] << std::endl;
+                    // std::cout << scan.ranges[i] << std::endl;
                     obstacle_flag = true;
                 }
             }
             if (obstacle_flag){
                 msg.linear.x = 0;
                 obstacle_pub.publish(msg);
-                std::cout << "Detect obstacle!\n";
+                std::cout << "Obstacle detected!\n";
                 obstacle_flag = false;
             }
 
@@ -49,6 +51,7 @@ class Chassis{
         double range_array[720] = {0};
         geometry_msgs::Twist msg;
 
+        double width = 1.355;
         double vel = 2;
         double acc = 2; // assuming acc == deceleration
 
@@ -56,11 +59,10 @@ class Chassis{
         double total_distance = 5;      // to be overwritten in constructor
         double lidar_distance = 0.1;
         double safe_distance = 0.3;
-        double width = 1.355;
 
         // times are in seconds
-        double sense_buffer = 0.025;      // since the lidar samples at 10 Hz
-        double comm_buffer = 0.05;
+        double sense_buffer = 1.0 / 40;    // since the lidar samples at 40 Hz
+        double comm_buffer = 0.025;
         double safe_buffer = 0.1;
 };
 
@@ -69,7 +71,12 @@ class Chassis{
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "obstacle_2d");
+    ros::NodeHandle n;
+
     Chassis bot;
+    bot.lidar_sub = n.subscribe("scan", 1000, &Chassis::obstacle_detection_callback, &bot);
+    bot.obstacle_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
     ros::Rate loop_rate(50);
 
     while(ros::ok()) {
